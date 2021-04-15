97ba8ac361cce0c596006df438fbe2aa21c4dda0
Acretock <georgoncharov@gmail.com>
2021-04-15 15:29:23 +0500

    Add project files.

---
+++ Week1-4-6/Week1-4-6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> res;
+    vector<int> t;
+    cin >> n;
+    while(n != 0)
+    {
+       
+        if (n % 2 == 0)
+        {
+            res.push_back(0);
+            n /= 2;
+        }
+        else
+        {
+            res.push_back(1);
+            n /= 2;
+        }
+    }
+    reverse(begin(res),end(res));
+    for (auto it = res.begin(); it != res.end(); ++it)
+        cout << *it;
+}
